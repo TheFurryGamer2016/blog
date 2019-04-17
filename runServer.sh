0d9dc8824cadbbe6c5cebfea3c5c52e50a93392c
Ben Leggiero <BenLeggiero@Gmail.com>
2019-04-16 23:56:33 -0400

    New post: "Objective-C was"

--- runServer.sh
+++ runServer.sh
@@ -1,1 +1,11 @@
-jekyll serve --trace --incremental
+
+bundle check
+bundleCheckStatus=$?
+
+if [ 0 -ne $bundleCheckStatus ]; then
+    printf "Updating Ruby gems...\n"
+    source updateGems.sh
+fi
+
+# jekyll serve --trace --incremental
+bundle exec jekyll serve --trace --incremental
